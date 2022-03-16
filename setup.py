59b44d6f554efb6a570838742b912ae9ff8193a7
anjavangestel <47362091+anjavangestel@users.noreply.github.com>
2022-03-16 12:18:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,14 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-# # read the API version from disk
-# with open(path.join(here, 'vantage6', 'tools', 'VERSION')) as fp:
-#     __version__ = fp.read()
-
 # setup the package
 setup(
-    name='v6-boilerplate-py',
+    name='v6-ROR_algo-py',
     version="1.0.0",
-    description='vantage6 biolerplate',
+    description='vantage6 ROR algorithm',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/IKNL/v6_boilerplate-py',
+    url='https://github.com/anjavangestel/v6-ROR_algo-py',
     packages=find_packages(),
     python_requires='>=3.6',
     install_requires=[
